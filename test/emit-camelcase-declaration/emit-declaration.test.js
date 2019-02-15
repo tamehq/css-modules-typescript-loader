8e9e438664ddbb2bea2d7004ad88f4baeabcacc6
fnky <chrs@cbp.io>
2019-02-15 18:57:37 +0100

    feat: Add camelCase option

---
+++ test/emit-camelcase-declaration/emit-declaration.test.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const compiler = require('../compiler.js');
+
+test('Can emit valid declaration', async () => {
+  await compiler(require.resolve('./index.js'), {
+    camelCase: true,
+  });
+
+  const declaration = fs.readFileSync(
+    require.resolve('./index.css.d.ts'),
+    'utf-8'
+  );
+
+  expect(declaration).toMatchSnapshot();
+});
